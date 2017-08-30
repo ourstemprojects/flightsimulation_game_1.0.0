df3fcd872336b01ff8495d7ed51c69f4155b5055
ourstemprojects <31186503+ourstemprojects@users.noreply.github.com>
2017-08-30 16:35:28 +0100

    Add files via upload

--- picode/testRPiGPIO.py
+++ picode/testRPiGPIO.py
@@ -2,6 +2,7 @@
 
 import time
 import random
+import DebugLogger as d
 
 BOARD = "board"
 BCM = "bcm"
@@ -10,27 +11,41 @@ IN = "in"
 RISING = "rising"
  
 def output(pin,value):
-  print "testRPiGPIO.output(pin, value): pin=%s, value=%s" % (pin, value)
+  d.debug_print("testRPiGPIO.output(pin, value): pin=%s, value=%s" % (pin, value))
 #endef
 
 def input(pin):
-  print "testRPiGPIO.input(pin): pin=%s" % (pin)
-  time.sleep(random.randint(1, 5))
-  return False
+
+  # wait a moment to simulate real-time nature of GPIO
+  time.sleep(1)
+  
+  # get a random number 0..20 and if it is less than 10 return false otherwise return true
+  value = random.randint(0, 20)
+  if value <= 10:
+    retval = False
+  else:
+    retval = True
+  #endif
+
+  # debug print pin and fake value
+  d.debug_print("testRPiGPIO.input(pin): pin=%s; level=%d" % (pin, retval))
+
+  # return the vale defined
+  return retval
 #enddef
  
 def setmode(mode):
-  print "testRPiGPIO.setmode(mode): mode=%s" % (mode)
+  d.debug_print("testRPiGPIO.setmode(mode): mode=%s" % (mode))
 #endef
  
 def setup(pin,value):
-  print "testRPiGPIO.setup(pin, value): pin=%s, value=%s" % (pin, value)
+  d.debug_print("testRPiGPIO.setup(pin, value): pin=%s, value=%s" % (pin, value))
 #endef  
 
 def add_event_detect(pin, direction, callback):
-  print "testRPiGPIO.add_event_detect(pin, direction, callback): pin=%s, direction=%s, callback=%s" % (pin, direction, str(callback))
+  d.debug_print("testRPiGPIO.add_event_detect(pin, direction, callback): pin=%s, direction=%s, callback=%s" % (pin, direction, str(callback)))
 #endef
  
 def cleanup():
-  print "testRPiGPIO.cleanup():"
+  d.debug_print("testRPiGPIO.cleanup():")
 #endef
